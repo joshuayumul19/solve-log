d82287b032e1f38763493114bfa3edb5f16c273d
joshy <108915592+joshuayumul19@users.noreply.github.com>
2024-09-04 15:56:01 +0800

    Project Setup : Backend
    - Installation of MongoDB / Atlas Connection

---
+++ backend/models/IssueModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema(
+	{
+		ticket_number: {
+			type: Number,
+			required: true,
+			unique: true,
+			index: true,
+		},
+		issue: {
+			type: String,
+			required: true,
+			text: true,
+			lowercase: true, // Add this line
+		},
+		status: {
+			type: String,
+			required: true,
+			index: true,
+		},
+		solution: {
+			type: String,
+			required: true,
+		},
+		reporter: {
+			type: String,
+			required: true,
+		},
+		date: {
+			type: Date,
+			required: true,
+			default: Date.now,
+			index: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+issueSchema.index({ issue: "text" });
+issueSchema.index({ date: -1 });
+
+const Issue = mongoose.model("Issue", issueSchema);
+
+export default Issue;

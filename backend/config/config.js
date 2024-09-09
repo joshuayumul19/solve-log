bad750fa47a2c575dac8b5b1b44c3c154ec1c378
joshy <108915592+joshuayumul19@users.noreply.github.com>
2024-09-09 23:44:26 +0800

    refactoring the backend setup using mono repo style

---
+++ backend/config/config.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config(); // Load environment variables from .env file
+
+//Database connection
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+export const connectToDB = async (app) => {
+	try {
+		const connect = await mongoose.connect(MONGO_URI);
+		app.listen(PORT);
+		console.log(`Database Connected: ${connect.connection.host}`);
+		console.log("Listening on port", PORT);
+	} catch (error) {
+		console.error(`Error ${error.message}`);
+		process.exit(1); //process code 1 means exit with failure, 0 means sucess
+	}
+};
